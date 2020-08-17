947d5b11d66fcaa818d203567a91f240fb90bbf5
eric <howdrazys@gmail.com>
2020-08-18 07:04:43 +0800

    first

---
+++ week04/components/loading.js
@@ -0,0 +1,9 @@
+
+
+Vue.component('loading', {
+    template: `
+        <div id="loading">
+        </div>
+    `,
+    props: ['loading'],
+})
---
+++ week04/components/pagination.js
@@ -0,0 +1,38 @@
+
+
+Vue.component('pagination', {
+    template: `
+    <nav>
+        <ul class="pagination">
+            <li @click.prevent="changePage(page.current_page -1)"
+                :class="{disabled: page.current_page <= 1 }"
+                class="page-item">
+                <a class="page-link" href="javascript:;">
+                    Previous
+                </a>
+            </li>
+            <li @click="changePage(n)"
+                :class = "{active : page.current_page === n}" 
+                v-for="n in page.total_pages" :key="n" 
+                class="page-item">
+                <a class="page-link" href="javascript:;">
+                    {{n}}
+                </a>
+            </li>
+            <li @click="changePage(page.current_page + 1)"
+                :class=" {disabled: page.current_page >= page.total_pages}"
+                class="page-item">
+                <a class="page-link" href="javascript:;">
+                    Next
+                </a>
+            </li>
+        </ul>
+    </nav>
+    `,
+    props: ['page'],
+    methods: {
+        changePage(page) {
+            this.$emit('e-change-page', page);
+        }
+    }
+})
